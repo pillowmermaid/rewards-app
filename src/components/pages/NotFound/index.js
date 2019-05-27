aa55fb52b0070d2d0020e74af7b90d0fb5ad54fb
Amir Ashtiani <pillowmermaid@gmail.com>
2019-05-26 22:46:32 -0400

    Style error page

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.css';
 
 const NotFound = () => {
     return (
-      <div className="page-not-found">
-        <h1>Error 404</h1>
+      <div className="page page--not-found">
+        <h1>Sorry, we cannot find your page</h1>
+        <Link to="/">Return to home page</Link>
       </div>
     );
 }
